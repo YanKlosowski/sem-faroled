5388b696583286c21bbdf29b3eb812f14617d034
Yan Marcel Alves Klosowski <yanalvez93@gmail.com>
2025-06-21 16:10:51 -0300

    Código Semáfaro

---
+++ semaled.ard/semaled.ard.ino
@@ -0,0 +1,38 @@
+//projeto por André Morais, obrigado pelo suporte.
+
+
+#define LEDverde 10
+#define LEDamarelo 9
+#define LEDvermelho 8
+
+
+void setup()
+{
+  pinMode (LEDvermelho, OUTPUT);
+  pinMode (LEDamarelo, OUTPUT);
+  pinMode (LEDverde, OUTPUT);
+}
+
+void loop()
+{
+  //ativação do LED verde
+  digitalWrite (LEDvermelho, LOW);
+  digitalWrite(LEDverde, HIGH);
+  delay(5000);
+  digitalWrite(LEDverde, LOW);
+  delay(500);
+  
+  //ativação do LED amarelo (oscilante)
+  {
+    digitalWrite(LEDamarelo, HIGH);
+    delay(2000);
+    digitalWrite(LEDamarelo, LOW);
+    delay(500);
+  }
+  
+  //ativação do LED vermelho
+  digitalWrite(LEDvermelho, HIGH);
+  delay(5000);
+  digitalWrite(LEDvermelho, LOW);
+  delay(500);
+}
